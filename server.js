288066f38fb868cf2f9f1f1bfc84c780973cdd67
Pere <pere.solaclaver@gmail.com>
2019-11-28 23:34:56 +0000

    Code to send email from contact form

--- server.js
+++ server.js
@@ -1,66 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const nodemailer = require("nodemailer");
 require("dotenv").config();
-// const { google } = require("googleapis");
+const mailgun = require("mailgun-js");
 
 const app = express();
-// const OAuth2 = google.auth.OAuth2;
-
-// const oauth2Client = new OAuth2(
-//   process.env.CLIENT_ID,
-//   process.env.CLIENT_SECRET,
-//   "https://developers.google.com/oauthplayground"
-// );
-
-// oauth2Client.setCredentials({
-//   refresh_token: process.env.REFRESH_TOKEN
-// });
-
-// const accessToken = oauth2Client.getAccessToken();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-const auth = {
-  type: "OAuth2",
-  user: 'pere.solaclaver@gmail.com',
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  refreshToken: process.env.REFRESH_TOKEN
-  // accessToken: accessToken
-};
+const DOMAIN = process.env.EMAIL_TOOL_DOMAIN;
+const mg = mailgun({ apiKey: process.env.EMAIL_TOOL_API_KEY, domain: DOMAIN });
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: `Hello from dummy endpoint` });
 });
 
 app.post("/contact", (req, res) => {
-  // Instantiate the SMTP server
-  const smtpTrans = nodemailer.createTransport({
-    service: "gmail",
-    auth: auth
-  });
-
-  // Specify what the email will look like
-  const mailOpts = {
-    from: req.body.email, // This is ignored by Gmail
-    to: 'pere.solaclaver@gmail.com',
-    subject: "New message from contact form at peresola.com",
-    // text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-    text: `this is a test email`
+  const { name, email, message } = req.body;
+  const data = {
+    from: email,
+    to: process.env.EMAIL_TEST_EMAIL,
+    subject: `Contact form from ${name}`,
+    text: message
   };
-
-  // Attempt to send the email
-  smtpTrans.sendMail(mailOpts, (error, response) => {
+  mg.messages().send(data, function(error, body) {
     if (error) {
       console.log(error);
-      // res.render("contact-failure"); // Show a page indicating failure
     } else {
-      // res.render("contact-success"); // Show a page indicating success
-      response.status(200).json({ message: `It worked` });
+      res.status(200).json({message: `yay`});
     }
   });
 });
