2c1ddf4979993859a9c5ff2ae3aa3725408fd434
Pere <pere.solaclaver@gmail.com>
2019-12-01 20:18:48 +0000

    Add middleware indicating where the static assets are

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-require("dotenv").config();
-const mailgun = require("mailgun-js");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
+const mailgun = require('mailgun-js');
 
 const app = express();
 
+app.use(express.static(__dirname + '../client/build'));
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
@@ -13,11 +14,11 @@ app.use(express.json());
 const DOMAIN = process.env.EMAIL_TOOL_DOMAIN;
 const mg = mailgun({ apiKey: process.env.EMAIL_TOOL_API_KEY, domain: DOMAIN });
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({ message: `Hello from dummy endpoint` });
 });
 
-app.post("/contact", (req, res) => {
+app.post('/contact', (req, res) => {
   const { name, email, message } = req.body;
   const data = {
     from: email,
@@ -29,7 +30,7 @@ app.post("/contact", (req, res) => {
     if (error) {
       console.log(error);
     } else {
-      res.status(200).json({message: `yay`});
+      res.status(200).json({ message: `yay` });
     }
   });
 });
