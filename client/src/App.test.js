83f795f398f9443e326437742ed63ed29910e282
Pere <pere.solaclaver@gmail.com>
2019-12-02 16:50:49 +0000

    Add --coverage flag to test script

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
-kimport React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { MemoryRouter  as Router } from 'react-router-dom';
 import App from './App';
 
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(
+    <Router>
+      <App />
+    </Router>
+  );
+});
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapper.queryByText(/Pere Sola Claver/i)).toBeInTheDocument();
 });
