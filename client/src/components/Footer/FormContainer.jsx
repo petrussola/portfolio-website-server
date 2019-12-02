5ac87f7db626be030804f3f651b3e55c5b08f188
Pere <pere.solaclaver@gmail.com>
2019-12-02 13:49:34 +0000

    Add env variable for monolith apps

--- client/src/components/Footer/FormContainer.jsx
+++ client/src/components/Footer/FormContainer.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import ContactUsForm from "./ContactUsForm";
-import axios from "axios";
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import ContactUsForm from './ContactUsForm';
+import axios from 'axios';
 
 // MATERIAL UI
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
-    width: "50%"
+    width: '50%'
   }
 });
 
 const initialValues = {
-  name: "",
-  email: "",
-  message: ""
+  name: '',
+  email: '',
+  message: ''
 };
 
 export default function FormContainer({ inFooter }) {
   const submitFormHandler = (values, action) => {
     axios
-      .post("http://localhost:4400/contact", values)
+      .post((process.env.REACT_APP_API || '') + '/contact', values)
       .then(data => {
-        alert("form has been sent");
+        alert('form has been sent');
         action.resetForm();
       })
       .catch(error => {
