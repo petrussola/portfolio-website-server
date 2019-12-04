f817efbba38773a05c398ac44196adcadc8ded53
Pere <pere.solaclaver@gmail.com>
2019-12-04 21:42:44 +0000

    end point to test env variable

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
-import "./App.css";
-import { Route } from "react-router-dom";
+import React, { useEffect } from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
 
 // IMPORT COMPONENTS
-import Header from "./components/Header/Header";
-import HomePage from "./components/HomePage";
-import About from "./components/About/About";
-import Project from "./components/ProjectPage/Project";
-import Center from "./components/Header/Center";
-import Footer from "./components/Footer/Footer";
-import ContactPage from "./components/Contact/ContactPage";
+import Header from './components/Header/Header';
+import HomePage from './components/HomePage';
+import About from './components/About/About';
+import Project from './components/ProjectPage/Project';
+import Center from './components/Header/Center';
+import Footer from './components/Footer/Footer';
+import ContactPage from './components/Contact/ContactPage';
+import Axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios
+      .get('http://localhost:4400/ssl')
+      .then(data => {
+        console.log(data.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
       <Route path="/" component={Header} />
